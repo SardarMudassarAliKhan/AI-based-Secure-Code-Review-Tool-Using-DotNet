54ac4da7f191f0ffb425d932aee2b506d00a77b8
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-11-12 01:02:37 +0500

    Add project files.

---
+++ AI-based-Secure-Code-Review-Tool/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AI_based_Secure_Code_Review_Tool.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ AI-based-Secure-Code-Review-Tool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI_based_Secure_Code_Review_Tool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
