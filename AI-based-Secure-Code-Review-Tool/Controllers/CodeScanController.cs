39267ae74894a353f0412bdbfe548810671b727b
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-11-14 00:40:50 +0500

    Update navigation and add CodeScan functionality
    Updated Index.cshtml and _Layout.cshtml to change navigation links and attributes for code submission and dashboard. Added CodeScanController with Scanner and MyDashBoard actions, both requiring authorization. Introduced Scanner.cshtml view with appropriate layout and title.

---
+++ AI-based-Secure-Code-Review-Tool/Controllers/CodeScanController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI_based_Secure_Code_Review_Tool.Controllers
+{
+    public class CodeScanController : Controller
+    {
+        [Authorize]
+        public IActionResult Scanner()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult MyDashBoard()
+        {
+            return View();
+        }
+    }
+}
